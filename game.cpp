c97ee821e6e9b66fff748611a3550c7af6daadcd
Aditya Suri <aditya.as.suri@gmail.com>
2025-09-26 20:21:50 -0400

    Added command line argument to choose number of layers

--- game.cpp
+++ game.cpp
@@ -2,8 +2,35 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cctype>
 
-int main() {
-    NxNCube cube(6);
-    cube.draw();
+bool isPositiveInt(std::string str) {
+  for (size_t i = 0; i < str.size(); ++i) {
+    if (!std::isdigit(str[i])) {
+      std::cout << str[i] << " is not a digit character" << std::endl;
+      return false;
+    }
+  }
+  return true;
+}
+
+int main(int argc, char *argv[]) {
+  std::string arg_error = "USAGE: ./game.exe LAYERS";
+  if (!(argc == 2)) {
+    std::cout << arg_error << std::endl;
+  }
+  if (!isPositiveInt(argv[1])) {
+    std::cout << arg_error << std::endl;
+    return 1;
+  }
+  if (std::stoi(argv[1]) == 0) {
+    std::cout << "LAYERS cannot be zero" << std::endl;
+    std::cout << arg_error << std::endl;
+    return 1;
+  }
+
+  int n = std::stoi(argv[1]);
+
+  NxNCube cube(n);
+  cube.draw();
 }
